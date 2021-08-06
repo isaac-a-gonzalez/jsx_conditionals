36aeccb909e654859d874e6066c147dc3091fd1f
Isaac A. Gonzalez <isaaca.gonzalez228@gmail.com>
2021-08-06 15:09:50 -0700

    alternate logic using if else statements

--- index.js
+++ index.js
@@ -5,22 +5,24 @@ function getNum() {
 class NumPicker extends React.Component {
   render() {
     const num = getNum();
+    let msg;
+    if (num === 7) {
+      msg = (
+        <div>
+          <h2>CONGRATS YOU WIN</h2>
+          <img
+            src="https://media4.giphy.com/media/nXxOjZrbnbRxS/giphy.webp?cid=ecf05e47i7tyyq1vjz2ixr57usq2fc6vyrjgcye49ascufyi&rid=giphy.webp&ct=g"
+            alt="man making 'yes' gesture"
+          />
+        </div>
+      );
+    } else {
+      msg = <p>Sorry You Lose</p>;
+    }
     return (
       <div>
-        <h1> Your number is: {num}</h1>
-        <p>{num === 7 ? "Congrats" : "Unlucky"}</p>
-        {/* {num === 7 ? (
-          <img
-            src="https://media4.giphy.com/media/nXxOjZrbnbRxS/giphy.webp?cid=ecf05e47i7tyyq1vjz2ixr57usq2fc6vyrjgcye49ascufyi&rid=giphy.webp&ct=g"
-            alt="man making 'yes' gesture"
-          />
-        ) : null} */}
-        {num == 7 && (
-          <img
-            src="https://media4.giphy.com/media/nXxOjZrbnbRxS/giphy.webp?cid=ecf05e47i7tyyq1vjz2ixr57usq2fc6vyrjgcye49ascufyi&rid=giphy.webp&ct=g"
-            alt="man making 'yes' gesture"
-          />
-        )}
+        <h1>Your number is: {num}</h1>
+        {msg}
       </div>
     );
   }
