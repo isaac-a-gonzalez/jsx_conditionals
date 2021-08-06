fcb902050708e8581e375fb290cf5201cb8a6e1e
Isaac A. Gonzalez <isaaca.gonzalez228@gmail.com>
2021-08-06 15:00:30 -0700

    secondary way to display gif

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ class NumPicker extends React.Component {
       <div>
         <h1> Your number is: {num}</h1>
         <p>{num === 7 ? "Congrats" : "Unlucky"}</p>
+        {/* {num === 7 ? (
+          <img
+            src="https://media4.giphy.com/media/nXxOjZrbnbRxS/giphy.webp?cid=ecf05e47i7tyyq1vjz2ixr57usq2fc6vyrjgcye49ascufyi&rid=giphy.webp&ct=g"
+            alt="man making 'yes' gesture"
+          />
+        ) : null} */}
+        {num == 7 && (
+          <img
+            src="https://media4.giphy.com/media/nXxOjZrbnbRxS/giphy.webp?cid=ecf05e47i7tyyq1vjz2ixr57usq2fc6vyrjgcye49ascufyi&rid=giphy.webp&ct=g"
+            alt="man making 'yes' gesture"
+          />
+        )}
       </div>
     );
   }
