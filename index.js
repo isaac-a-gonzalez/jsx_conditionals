a5ffb12fd52c1769f38d5f5a4da92b8aaa65fb68
Isaac A. Gonzalez <isaaca.gonzalez228@gmail.com>
2021-08-06 14:35:42 -0700

    added random number genorator function. works in live server

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+//function to find a random number
+function getNum() {
+  return Math.floor(Math.random() * 10) + 1;
+}
 class NumPicker extends React.Component {
-  render() {}
+  render() {
+    return <h1>Your number is {getNum()}</h1>;
+  }
 }
 
 ReactDOM.render(<NumPicker />, document.getElementById("root"));
